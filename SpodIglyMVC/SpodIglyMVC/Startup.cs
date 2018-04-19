e93de6849f0eac36703ceb6acb5698feb5af8506
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-19 18:55:51 +0100

    W trakcie

---
+++ SpodIglyMVC/SpodIglyMVC/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+using Hangfire.SqlServer;
+using Hangfire.Dashboard;
+
+[assembly: OwinStartup(typeof(SpodIglyMVC.Startup))]
+
+namespace SpodIglyMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.UseHangfire(config =>
+            {
+                config.UseAuthorizationFilters(new AuthorizationFilter
+                {
+                    Roles = "Admin"
+                });
+
+                config.UseSqlServerStorage("StoreContext");
+                config.UseServer();
+            });
+        }
+    }
+}
