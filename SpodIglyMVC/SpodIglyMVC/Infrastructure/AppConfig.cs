f9e8651baacdf72567919171fceec96a8202d2d1
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-25 14:15:53 +0100

    Strona główna

---
+++ SpodIglyMVC/SpodIglyMVC/Infrastructure/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class AppConfig
+    {
+        public static string _genreIconsFolderRelative = ConfigurationManager.AppSettings["GenreIconsFolder"];
+        public static string GenreIconsFolderRelative { get { return _genreIconsFolderRelative; } }
+        
+
+        public static string _photosFolderRelative = ConfigurationManager.AppSettings["PhotosFolder"];
+        public static string PhotosFolderRelative { get { return _photosFolderRelative; } }
+    }
+}
