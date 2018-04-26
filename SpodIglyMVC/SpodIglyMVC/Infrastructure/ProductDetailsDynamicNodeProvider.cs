d70c50a7e2c00ffcaeb69a1f2e5fe789cfd3ce90
SKarpiuKursy <sylwester.karpiuk@hotmail.com>
2018-04-26 20:27:30 +0100

    Pasek nawigacyjny

---
+++ SpodIglyMVC/SpodIglyMVC/Infrastructure/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,34 @@
+﻿using MvcSiteMapProvider;
+using SpodIglyMVC.DAL;
+using SpodIglyMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class ProductDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext _db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            // Build value 
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Album a in _db.Albums)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = a.AlbumTitle;
+                n.Key = "Album_" + a.AlbumId;
+                n.ParentKey = "Genre_" + a.GenreId;
+                n.RouteValues.Add("id", a.AlbumId);
+                returnValue.Add(n);
+            }
+
+            // Return 
+            return returnValue;
+        }
+    }
+}
---
+++ SpodIglyMVC/SpodIglyMVC/Infrastructure/ProductListDynamicNodeProvider.cs
@@ -0,0 +1,33 @@
+﻿using MvcSiteMapProvider;
+using SpodIglyMVC.DAL;
+using SpodIglyMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class ProductListDynamicNodeProvider:DynamicNodeProviderBase
+    {
+        private StoreContext _db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            // Build value 
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Genre g in _db.Genres)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = g.Name;
+                n.Key = "Genre_" + g.GenreId;
+                n.RouteValues.Add("genrename", g.Name);
+                returnValue.Add(n);
+            }
+
+            // Return 
+            return returnValue;
+        }
+    }
+}
